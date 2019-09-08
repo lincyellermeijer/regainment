d22d1137a7acdc44db6c1969f7aece4a8eaf20f8
Lincy Ellermeijer <lincy_95@hotmail.com>
2019-09-08 20:04:09 +0200

    Add script to make the different background elements move at different speeds related to the cam, to create parallax effect

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+
+    // set and get in editor
+    [SerializeField] public GameObject cam;
+    [SerializeField] public float parallaxEffect;
+
+
+    void Start()
+    {
+        // get start position of background element and the total length
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+
+    void Update()
+    {
+        // make a temp float to keep track of the current position and the distance float to determine how fast it should move
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        // set new position according to the startposition, distance and the parallax effect
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        // when the element will get out of bounds, reset position
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+
+
+}
