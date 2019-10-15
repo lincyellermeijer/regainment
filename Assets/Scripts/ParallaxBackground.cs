b4954b02fc8d24d729dbd65a9b8161d6b552a2b8
Lincy Ellermeijer <lincy_95@hotmail.com>
2019-10-15 15:18:23 +0200

    Updated script to work with camera size and not be dependent on the length of the sprite. New parallax background fully implemented and working!

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private float length, startpos;
+
+    // set and get in editor
+    [SerializeField] public Camera cam;
+    [SerializeField] public float parallaxEffect;
+
+
+    void Start()
+    {
+        // get width and height of the camera
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        // get start position of background element and the total length
+        startpos = transform.position.x;
+        length = width;
+        // get length of background element
+            //GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+
+    void Update()
+    {
+        // make a temp float to keep track of the current position and the distance float to determine how fast it should move
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        // set new position according to the startposition, distance and the parallax effect
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        // when the element will get out of bounds, reset position
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+
+
+}
